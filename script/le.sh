4f1380a34331e980d6d313af22221948bbe81353
Aleksandr <shepeliev@users.noreply.github.com>
2018-03-06 21:27:26 +0200

    avoid renewing certs by default

--- script/le.sh
+++ script/le.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$LETSENCRYPT" = "true" ]; then
-    certbot certonly -t -n --agree-tos --renew-by-default --email "${LE_EMAIL}" --webroot -w /usr/share/nginx/html -d ${LE_FQDN}
+    certbot certonly -t -n --agree-tos --keep-until-expiring --email "${LE_EMAIL}" --webroot -w /usr/share/nginx/html -d ${LE_FQDN}
     FIRST_FQDN=$(echo "$LE_FQDN" | cut -d"," -f1)
     cp -fv /etc/letsencrypt/live/${FIRST_FQDN}/privkey.pem /etc/nginx/ssl/le-key.pem
     cp -fv /etc/letsencrypt/live/${FIRST_FQDN}/fullchain.pem /etc/nginx/ssl/le-crt.pem
