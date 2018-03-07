d6e333de4e4ccaec3ac7350e8788e77ad64017ac
Oleksandr Shepeliev <a.shepeliev@gmail.com>
2018-03-07 12:06:46 +0200

    Make it compatible with Swarm configs

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -5,12 +5,6 @@ echo "start nginx"
 cp /usr/share/zoneinfo/${TZ} /etc/localtime && \
 echo ${TZ} > /etc/timezone && \
 
-#setup ssl keys
-echo "ssl_key=${SSL_KEY:=le-key.pem}, ssl_cert=${SSL_CERT:=le-crt.pem}, ssl_chain_cert=${SSL_CHAIN_CERT:=le-chain-crt.pem}"
-SSL_KEY=/etc/nginx/ssl/${SSL_KEY}
-SSL_CERT=/etc/nginx/ssl/${SSL_CERT}
-SSL_CHAIN_CERT=/etc/nginx/ssl/${SSL_CHAIN_CERT}
-
 mkdir -p /etc/nginx/conf.d
 mkdir -p /etc/nginx/ssl
 
@@ -18,11 +12,6 @@ mkdir -p /etc/nginx/ssl
 if [ -f /etc/nginx/service*.conf ]; then
     cp -fv /etc/nginx/service*.conf /etc/nginx/conf.d/
 fi
-
-#replace SSL_KEY, SSL_CERT and SSL_CHAIN_CERT by actual keys
-sed -i "s|SSL_KEY|${SSL_KEY}|g" /etc/nginx/conf.d/*.conf
-sed -i "s|SSL_CERT|${SSL_CERT}|g" /etc/nginx/conf.d/*.conf
-sed -i "s|SSL_CHAIN_CERT|${SSL_CHAIN_CERT}|g" /etc/nginx/conf.d/*.conf
 
 #generate dhparams.pem
 if [ ! -f /etc/nginx/ssl/dhparams.pem ]; then
@@ -32,8 +21,8 @@ if [ ! -f /etc/nginx/ssl/dhparams.pem ]; then
     chmod 600 dhparams.pem
 fi
 
-#disable ssl configuration and let it run without SSL
-mv -v /etc/nginx/conf.d /etc/nginx/conf.d.disabled
+#disable service configurations and let it run without SSL
+sed -i "s|include /etc/nginx/conf.d/\*.conf;|# include /etc/nginx/conf.d/\*.conf;|g" /etc/nginx/nginx.conf
 
 (
  sleep 5 #give nginx time to start
@@ -43,11 +32,12 @@ mv -v /etc/nginx/conf.d /etc/nginx/conf.d.disabled
 	echo "trying to update letsencrypt ..."
     /le.sh
     rm -f /etc/nginx/conf.d/default.conf 2>/dev/null #remove default config, conflicting on 80
-    mv -v /etc/nginx/conf.d.disabled /etc/nginx/conf.d #enable
+    sed -i "s|# include /etc/nginx/conf.d/\*.conf;|include /etc/nginx/conf.d/\*.conf;|g" /etc/nginx/nginx.conf # enable
     echo "reload nginx with ssl"
     nginx -s reload
     sleep 60d
  done
 ) &
+
 
 nginx -g "daemon off;"
